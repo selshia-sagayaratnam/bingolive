43730106ea1f424fbdf082aeb3d3f20232504050
Selshia Sagayaratnam <49331578+selshia-sagayaratnam@users.noreply.github.com>
2026-02-10 09:23:45 +0530

    export cont supabase

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,6 +3,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://qgrmahhlxqantljfsvix.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncm1haGhseHFhbnRsamZzdml4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NDA2MTksImV4cCI6MjA4NjIxNjYxOX0.FWHU_GED_vwqzA5gWb5obP36zHDUtuQZ5yRfDGlykIU'
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!
 )
